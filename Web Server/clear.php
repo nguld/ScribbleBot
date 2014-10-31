e2a9f8af379902438d2d4d31222980c6e41502f9
martvanburen <mart.van.buren@gmail.com>
2014-10-31 09:52:41 -0400

    Upload New WebServer Files

--- Web Server/clear.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-file_put_contents("output/commands.txt", "");
-
-?>
--- Web Server/clearline.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$file = file_get_contents("output/commands.txt");
-$arr = explode('\n\r', $file);
-if (isset($arr[0])) unset ($arr[0]);
-$string = implode('\n\r', $arr);
-file_put_contents("output/commands.txt", $string);
-
-?>
