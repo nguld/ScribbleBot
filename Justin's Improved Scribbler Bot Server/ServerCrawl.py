8ce82a02217a07746bfba6ca2af93e53ed464ae8
jastribl <jastribl@uwaterloo.ca>
2014-11-16 03:02:28 -0500

    Added Updated Server Crawl
    Added Updated Server Crawl to no longer use Server (haha) and use Redis

--- Justin's Improved Scribbler Bot Server/ServerCrawl.py
+++ Justin's Improved Scribbler Bot Server/ServerCrawl.py
@@ -1,78 +1,80 @@
-#http://noahguld.com/scribblerBot/
-
-from MarioSounds import *
+from Parser import parse
+from speakCustom import *
+from Sounds import *
 from FindObject import *
-from AroundBox import *
-import urllib2
 from Queue import *
 import threading
-import thread
+import subprocess
+import sys
 
 queue = Queue()
 
-class updateQueueThread (threading.Thread):
-    def __init__(self, threadID, name):
+class newThread (threading.Thread):
+    def __init__(self, function):
         threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
+        self.function = function
     def run(self):
-        print "Starting " + self.name
-        updateQueue()
-        print "Exiting " + self.name
+        self.function()
 
-class runQueueThread (threading.Thread):
-    def __init__(self, threadID, name):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-    def run(self):
-        print "Starting " + self.name
-        runQueue()
-        print "Exiting " + self.name
+# def updateCommand():
+#     a = subprocess.Popen(["java", "-jar", "JavaRedis.jar", "pub-redis-16825.us-east-1-2.5.ec2.garantiadata.com", "16825", "GiJiJuKaMaNoRo", "scribblerCommands"], shell=False, stdout=subprocess.PIPE)
+#     while (True):
+#         sys.stdout.write(a.stdout.readline())
 
+# def updateMyoPoseData():
+#     b = subprocess.Popen(["java", "-jar", "javaredis.jar", "pub-redis-10683.us-east-1-2.5.ec2.garantiadata.com ", "10683", "GiJiJuKaMaNoRo", "scribblerMyoPoses"], shell=False, stdout=subprocess.PIPE)
+#     while (True):
+#         pyoPose = c.stdout.readline()
+#         print pyoPose
+
+# def updateMyoSensorData():
+#     c = subprocess.Popen(["java", "-jar", "javaredis.jar", "pub-redis-10592.us-east-1-2.5.ec2.garantiadata.com", "10592", "GiJiJuKaMaNoRo", "scribblerMyoSensors"], shell=False, stdout=subprocess.PIPE)
+#     while (True):
+#         myoSensors = d.stdout.readline()
+#         print myoSensors
 
 def updateQueue():
+    a = subprocess.Popen(["java", "-jar", "JavaRedis.jar", "pub-redis-16825.us-east-1-2.5.ec2.garantiadata.com", "16825", "GiJiJuKaMaNoRo", "scribblerCommands"], shell=False, stdout=subprocess.PIPE)
     while True:
-        #print "Update Queue"
-        data = urllib2.urlopen("http://www.noahguld.com/scribblerBot/output/commands.txt")
-
-        for line in data:
-            #print line
-            if line == "stopCurrent()\n":
-                print "Stopping Current Process"
-                urllib2.urlopen("http://www.noahguld.com/scribblerBot/clearline.php")
+        text = a.stdout.readline()
+        if(text!=""):
+            parsed = parse(text)
+            if parsed == "stopCurrent()":
+                try:
+                    stop()
+                except:
+                    pass
                 init("com3")
-                newRunThread = runQueueThread(2, "runThread")
-                newRunThread.start()
-            elif  line == "stopAll()\n":
-                print "Restarting"
-                urllib2.urlopen("http://www.noahguld.com/scribblerBot/clear.php")
+                aNewThread = newThread(runQueue)
+                aNewThread.start()
+            elif  parsed == "stopAll()":
+                try:
+                    stop()
+                except:
+                    pass
                 init("com3")
                 while queue.empty() == False:
                     queue.get()
-                newRunThread = runQueueThread(2, "runThread")
-                newRunThread.start()
+                aNewThread = newThread(runQueue)
+                aNewThread.start()
             else:
-                queue.put(line)
-            urllib2.urlopen("http://www.noahguld.com/scribblerBot/clearline.php")
+                queue.put(parsed)
 
 def runQueue():
     while True:
-        #print "Run Queue"
         while queue.empty() == False:
-            #if clearQueue != 1:
             exec queue.get()
 
+# compassThread = newThread (updateCompass)
+# commandThread = newThread (updateCommand)
+# poseThread = newThread(3, "poseThread")
+# sensorsThread = newThread(4, "sensorsThread")
+updateThread = newThread(updateQueue)
+runThread = newThread(runQueue)
 
-
-
-#threads[0] = thread.start_new_thread(updateQueue, ())
-#threads[1] = thread.start_new_thread(runQueue, ())
-#thread1 = StoppableThread(thread.start_new_thread(runQueue, ()))
-#thread.start_new_thread(updateQueue, ())
-#thread.start_new_thread(runQueue, ())
-updateThread = updateQueueThread(1, "updateThread")
-runThread = runQueueThread(2, "runThread")
-
+# compassThread.start()
+# commandThread.start()
+# poseThread.start()
+# sensorsThread.start()
 updateThread.start()
 runThread.start()
